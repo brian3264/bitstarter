b64275e871b3c08fd518d7393def92d6b5afd738
Brian Hicks <brian@nkhata.com>
2013-07-09 14:27:46 +0000

    Undo read index.html

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.writeFile(index.html, 'utf8');
+  response.send("Hello World2!");
 });
 
 var port = process.env.PORT || 5000;
