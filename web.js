e7f5ba99e973915f74c575404e320803f202f0ce
Brian Hicks <brian@nkhata.com>
2013-07-10 13:44:48 +0000

    Updated web.js to read index.html re edit2

--- web.js
+++ web.js
@@ -1,8 +1,15 @@
-var fs = require('fs');
 var express = require('express');
+
 var app = express.createServer(express.logger());
-var buf = fs.readFileSync('index.html','utf-8');
-var string = buf.toString();
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send(string);
+   var buf = fs.readFileSync('index.html');
+   
+   response.send(buf.toString());
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
