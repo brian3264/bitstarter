6aa0d7ae453fecf0a833f4b1be735e88f5451f56
Brian Hicks <brian@nkhata.com>
2013-07-10 08:27:27 +0000

    Updated web.js to read index.html

--- web.js
+++ web.js
@@ -1,10 +1,8 @@
-var fs = require('fs');
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 var buf = fs.readFileSync('index.html','utf-8');
 var string = buf.toString();
-
 app.get('/', function(request, response) {
   response.send(string);
 });
